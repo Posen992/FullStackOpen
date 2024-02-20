14c995b48dbea13109647414d3caf8dc0dea4520
Posen992 <chen.Posen992@gmail.com>
2024-02-20 02:23:06 +0200

    Upload the exercises 3.7 - 3.8
    Upload the exercises 3.7 - 3.8

--- Exercises/Part 3/phonebook-backend/index.js
+++ Exercises/Part 3/phonebook-backend/index.js
@@ -1,28 +1,41 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
 
-let persons =[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+morgan.token('body', function (req, res) { return req.body })
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        JSON.stringify(tokens['body'](req, res))
+    ].join(' ')
+}))
+
+let persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
@@ -42,7 +55,7 @@ app.get('/api/persons/:id', (request, response) => {
 
     if (person) {
         response.json(person)
-    }else{
+    } else {
         response.status(404).end()
     }
 })
@@ -57,9 +70,9 @@ app.delete('/api/persons/:id', (request, response) => {
 const generateId = () => {
     const id = Math.floor(Math.random() * 100000000);
 
-    if (persons.filter(person => person.id === id).length > 0){
+    if (persons.filter(person => person.id === id).length > 0) {
         generateId()
-    }else{
+    } else {
         return id
     }
 }
@@ -79,7 +92,7 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    if (persons.filter(person => person.name === body.name).length>0) {
+    if (persons.filter(person => person.name === body.name).length > 0) {
         return response.status(400).json({
             error: 'name must be unique'
         })
@@ -96,6 +109,6 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 const PORT = 3003
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
