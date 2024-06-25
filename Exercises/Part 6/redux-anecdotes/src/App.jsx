55d46a85d00dbd5e95f111ca8719cda166a717f9
Posen992 <chen.Posen992@gmail.com>
2024-06-25 22:44:21 +0300

    Upload exercises 6.3-6.8

---
+++ Exercises/Part 6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,13 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+
+const App = () => {
+  return (
+    <div>
+      <AnecdoteList></AnecdoteList>
+      <AnecdoteForm></AnecdoteForm>
+    </div>
+  )
+}
+
+export default App
